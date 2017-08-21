457ed41fc4540e2ea0ce0a54cc0f60f40fed85f7
xiaominfc@126.com <793101503@qq.com>
2017-08-21 10:39:18 +0800

    update make_'mariadb.sh

--- server/src/make_mariadb.sh
+++ server/src/make_mariadb.sh
@@ -2,12 +2,15 @@
 # author: luoning
 # date: 03/24/2015
 
+
+MARIADB_VERSION=10.0.17
+
 MARIADB_DEVEL=MariaDB-10.0.17-centos6-x86_64-devel
-MARIADB_DEVEL_DOWNLOAD_PATH=http://sfo1.mirrors.digitalocean.com/mariadb/mariadb-10.0.17/yum/centos6-amd64/rpms/$MARIADB_DEVEL.rpm
+MARIADB_DEVEL_DOWNLOAD_PATH=http://archive.mariadb.com/mariadb-10.0.17/yum/rhel6-amd64/rpms/$MARIADB_DEVEL.rpm
 MARIADB_COMMON=MariaDB-10.0.17-centos6-x86_64-common
-MARIADB_COMMON_DOWNLOAD_PATH=http://sfo1.mirrors.digitalocean.com/mariadb/mariadb-10.0.17/yum/centos6-amd64/rpms/$MARIADB_COMMON.rpm
+MARIADB_COMMON_DOWNLOAD_PATH=http://archive.mariadb.com/mariadb-10.0.17/yum/rhel6-amd64/rpms/$MARIADB_COMMON.rpm
 MARIADB_COMPAT=MariaDB-10.0.17-centos6-x86_64-compat
-MARIADB_COMPAT_DOWNLOAD_PATH=http://sfo1.mirrors.digitalocean.com/mariadb/mariadb-10.0.17/yum/centos6-amd64/rpms/$MARIADB_COMPAT.rpm
+MARIADB_COMPAT_DOWNLOAD_PATH=http://archive.mariadb.com/mariadb-10.0.17/yum/rhel6-amd64/rpms/$MARIADB_COMPAT.rpm
 CUR_DIR=
 
 check_user() {
