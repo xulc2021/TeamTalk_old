0600edd49e884f49f4753b49d125d17393b0c640
xiaominfc <xiaominfc@126.com>
2018-04-30 11:59:14 +0800

    update sql IMTransmitFile table

--- auto_setup/mariadb/conf/ttopen.sql
+++ auto_setup/mariadb/conf/ttopen.sql
@@ -503,7 +503,7 @@ CREATE TABLE `IMTransmitFile` (
   `fromId` int(11) unsigned NOT NULL COMMENT '发送用户的id',
   `toId` int(11) unsigned NOT NULL COMMENT '接收用户的id',
   `fileName` varchar(4096) COLLATE utf8mb4_bin DEFAULT '' COMMENT '文件名称',
-  `size` int(11) unsigned NOT NULL  COMMENT '文件大小',
+  `taskId` varchar(128) COLLATE utf8mb4_bin DEFAULT '' COMMENT 'taskId',
   `taskId` int(11) unsigned NOT NULL COMMENT 'taskId',
   `status` tinyint(1) unsigned NOT NULL DEFAULT '0' COMMENT '0正常 1被删除',
   `updated` int(11) unsigned NOT NULL COMMENT '更新时间',
---
+++ ios/TeamTalk/Module/TCPAPI/HasOfflineFileAPI.m
@@ -0,0 +1,13 @@
+//
+//  HasOfflineFileAPI.m
+//  TeamTalk
+//
+//  Created by xiaominfc on 2018/4/28.
+//  Copyright © 2018 MoguIM. All rights reserved.
+//
+
+#import "HasOfflineFileAPI.h"
+
+@implementation HasOfflineFileAPI
+
+@end
