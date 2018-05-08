2a9b8b13737cf6fa9f3ebacc22727c2f74aba517
xiaominfc <xiaominfc@126.com>
2018-05-08 12:02:49 +0800

    update code

--- server/run/restart.sh
+++ server/run/restart.sh
@@ -2,6 +2,11 @@
 #start or stop the im-server
 
 
+curpath=$(pwd)
+
+LD_LIBRARY_PATH=$curpath/lib
+export LD_LIBRARY_PATH="$LD_LIBRARY_PATH"
+
 function restart() {
     cd $1
     if [ ! -e *.conf  ]
@@ -31,29 +36,32 @@ function restart() {
 }
 
 case $1 in
-	login_server)
-		restart $1
-		;;
-	msg_server)
-		restart $1
-		;;
-	route_server)
-		restart $1
-		;;
-	http_msg_server)
-		restart $1
-		;;
-	file_server)
-		restart $1
-		;;
-  push_server)
-    restart $1
-    ;;
-  db_proxy_server)
-  restart $1
-  ;;
-	*)
-		echo "Usage: "
-		echo "  ./restart.sh (login_server|msg_server|route_server|http_msg_server|file_server|push_server)"
-		;;
+    login_server)
+        restart $1
+        ;;
+    msg_server)
+        restart $1
+        ;;
+    route_server)
+        restart $1
+        ;;
+    http_msg_server)
+        restart $1
+        ;;
+    file_server)
+        restart $1
+        ;;
+    push_server)
+        restart $1
+        ;;
+    db_proxy_server)
+        restart $1
+        ;;
+    msfs)
+        restart $1
+        ;;
+    *)
+        echo "Usage: "
+        echo "  ./restart.sh (login_server|msg_server|route_server|http_msg_server|file_server|push_server)"
+        ;;
 esac
