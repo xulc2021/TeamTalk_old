140ef81af7d69594853d3c5e8c420346c4e5c708
xiaominfc <xiaominfc@126.com>
2018-05-08 10:35:55 +0800

    update code for docker compile

--- docker/build.sh
+++ docker/build.sh
@@ -1,20 +1,34 @@
 #!/bin/bash
 
+
+im_server=im_server
+
 srcpaths=(slog base login_server db_proxy_server push_server route_server file_server msg_server msfs http_msg_server)
-base_src_path=/opt/src
+base_path=$(pwd)
+base_src_path=$base_path/../server/src
+
+out_path=$base_path/$im_server
+
+rm -rf $base_src_path/base/pb/protocol/*
+
+protoc -I=$base_path/../pb --cpp_out=$base_src_path/base/pb/protocol/ $base_path/../pb/*.proto
+
 cd $base_src_path
+
+find . -name "CMakeCache.txt" -exec rm -rf {} \;
+
 
 for sub_src_path in ${srcpaths[@]}
 do
     cd $sub_src_path
+    make clean
     cmake .
     make
     cd ..
 done
 
-
-mkdir -p /opt/im_server/lib
-cp /opt/src/slog/libslog.* /opt/im_server/lib/
+mkdir -p $out_path/lib
+cp $base_src_path/slog/libslog.* $out_path/lib/
 
 delete=(slog base)
 
@@ -25,9 +39,9 @@ done
 
 for server in ${srcpaths[@]}
 do
-    mkdir -p /opt/im_server/$server
-    cp -f /opt/src/$server/$server /opt/im_server/$server/$server
-    cp -f /opt/src/$server/*.conf /opt/im_server/$server/
-    cp -f /opt/src/$server/log4cxx.properties /opt/im_server/$server/
+    mkdir -p $build_path/$server
+    cp -f $base_src_path/$server/$server $out_path/$server/$server
+    cp -f $base_src_path/$server/*.conf $out_path/$server/
+    cp -f $base_src_path/slog/log4cxx.properties $out_path/$server/
 done
 
--- docker/docker_build.sh
+++ docker/docker_build.sh
@@ -1,16 +1,8 @@
 #!/bin/bash
-srcpath=../server/src/
-codepaths=(slog base login_server db_proxy_server push_server route_server file_server msg_server msfs http_msg_server)
-mkdir src
 
-for codepath in ${codepaths[@]}
-do
-    cp -rf $srcpath$codepath ./src/
-done
+project_path=$(pwd)/../
 
-cp -r ../pb ./src/
-rm -rf ./src/base/pb/protocol/*
-
+echo $project_path
 
 docker_image_name=xiaominfc/centos_tt_test
 docker_file=./Dockerfile
@@ -18,7 +10,6 @@ docker_file=./Dockerfile
 docker kill $(docker ps -q)
 docker build --rm -t $docker_image_name -f $docker_file . 
 
-#docker run -d  --privileged=true  -v /sys/fs/cgroup:/sys/fs/cgroup $docker_image_name /usr/sbin/init
-docker run -d  --privileged=true  -v /sys/fs/cgroup:/sys/fs/cgroup $docker_image_name
+docker run -d  --privileged=true -v "$project_path:/opt/tt_source_code"   $docker_image_name
 docker exec -it $(docker ps -q) /bin/bash
 
