449f0fcf0a1185c198b4a5a4b046b11609a24ee7
xmcy0011 <xmcy0011@sina.com>
2019-01-23 16:38:03 +0800

    bug fixed：密码输入错误超10次后，等待5分钟以上也不可登录的问题

--- server/src/db_proxy_server/business/Login.cpp
+++ server/src/db_proxy_server/business/Login.cpp
@@ -45,101 +45,105 @@ void doLogin(CImPdu* pPdu, uint32_t conn_uuid)
         
         msgResp.set_user_name(strDomain);
         msgResp.set_attach_data(msg.attach_data());
-        
-        do
-        {
-            CAutoLock cAutoLock(&g_cLimitLock);
-            list<uint32_t>& lsErrorTime = g_hmLimits[strDomain];
-            uint32_t tmNow = time(NULL);
-            
-            //清理超过30分钟的错误时间点记录
-            /*
-             清理放在这里还是放在密码错误后添加的时候呢？
-             放在这里，每次都要遍历，会有一点点性能的损失。
-             放在后面，可能会造成30分钟之前有10次错的，但是本次是对的就没办法再访问了。
-             */
-            auto itTime=lsErrorTime.begin();
-            for(; itTime!=lsErrorTime.end();++itTime)
-            {
-                if(tmNow - *itTime > 30*60)
-                {
-                    break;
-                }
-            }
-            if(itTime != lsErrorTime.end())
-            {
-                lsErrorTime.erase(itTime, lsErrorTime.end());
-            }
-            
-            // 判断30分钟内密码错误次数是否大于10
-            if(lsErrorTime.size() > 10)
-            {
-                itTime = lsErrorTime.begin();
-                if(tmNow - *itTime <= 30*60)
-                {
-                    msgResp.set_result_code(6);
-                    msgResp.set_result_string("用户名/密码错误次数太多");
-                    pPduResp->SetPBMsg(&msgResp);
-                    pPduResp->SetSeqNum(pPdu->GetSeqNum());
-                    pPduResp->SetServiceId(IM::BaseDefine::SID_OTHER);
-                    pPduResp->SetCommandId(IM::BaseDefine::CID_OTHER_VALIDATE_RSP);
-                    CProxyConn::AddResponsePdu(conn_uuid, pPduResp);
-                    return ;
-                }
-            }
-        } while(false);
-        
-        log("%s request login.", strDomain.c_str());
-        
-        
-        
-        IM::BaseDefine::UserInfo cUser;
-        
-        if(g_loginStrategy.doLogin(strDomain, strPass, cUser))
-        {
-            IM::BaseDefine::UserInfo* pUser = msgResp.mutable_user_info();
-            pUser->set_user_id(cUser.user_id());
-            pUser->set_user_gender(cUser.user_gender());
-            pUser->set_department_id(cUser.department_id());
-            pUser->set_user_nick_name(cUser.user_nick_name());
-            pUser->set_user_domain(cUser.user_domain());
-            pUser->set_avatar_url(cUser.avatar_url());
-            
-            pUser->set_email(cUser.email());
-            pUser->set_user_tel(cUser.user_tel());
-            pUser->set_user_real_name(cUser.user_real_name());
-            pUser->set_status(0);
+       
+	//清理超过5分钟的错误时间点记录
+	/*
+		清理放在这里还是放在密码错误后添加的时候呢？
+		放在这里，每次都要遍历，会有一点点性能的损失。
+		放在后面，可能会造成30分钟之前有10次错的，但是本次是对的就没办法再访问了。
+	*/
+	{
+	    CAutoLock cAutoLock(&g_cLimitLock);
+	    list<uint32_t>& lsErrorTime = g_hmLimits[strDomain];
+	    int kTimeLimit = 5 * 60;
+	    list<uint32_t> temp_list;
+	    uint32_t tmNow = time(NULL);
 
-            pUser->set_sign_info(cUser.sign_info());
-           
-            msgResp.set_result_code(0);
-            msgResp.set_result_string("成功");
-            
-            //如果登陆成功，则清除错误尝试限制
-            CAutoLock cAutoLock(&g_cLimitLock);
-            list<uint32_t>& lsErrorTime = g_hmLimits[strDomain];
-            lsErrorTime.clear();
-        }
-        else
-        {
-            //密码错误，记录一次登陆失败
-            uint32_t tmCurrent = time(NULL);
-            CAutoLock cAutoLock(&g_cLimitLock);
-            list<uint32_t>& lsErrorTime = g_hmLimits[strDomain];
-            lsErrorTime.push_front(tmCurrent);
-            
-            log("get result false");
-            msgResp.set_result_code(1);
-            msgResp.set_result_string("用户名/密码错误");
-        }
+	    // 判断5分钟内密码错误次数是否大于10
+	    if (lsErrorTime.size() > 10) {
+                for (auto itTime = lsErrorTime.begin(); itTime != lsErrorTime.end(); ++itTime)
+		{
+		    if ((tmNow - *itTime) < kTimeLimit) // 淘汰早于5分钟前的
+		    {
+		        temp_list.push_back(*itTime);
+		    }
+		}
+			
+		uint32_t last_error_time = temp_list.front(); // 倒序
+		if (tmNow - last_error_time <= kTimeLimit) {
+		    sgResp.set_result_code(6);
+		    msgResp.set_result_string("用户名/密码错误次数太多");
+		    pPduResp->SetPBMsg(&msgResp);
+		    pPduResp->SetSeqNum(pPdu->GetSeqNum());
+		    pPduResp->SetServiceId(IM::BaseDefine::SID_OTHER);
+		    pPduResp->SetCommandId(IM::BaseDefine::CID_OTHER_VALIDATE_RSP);
+		    CProxyConn::AddResponsePdu(conn_uuid, pPduResp);
+
+		    // 加入
+		    if(temp_list.size() > 10)
+		        temp_list.pop_back();		// 删除最早时候的错误时间
+		    temp_list.push_front(tmNow);	// 更新这次错误时间
+		    lsErrorTime.clear();
+		    for (auto itTime = temp_list.begin(); itTime != temp_list.end(); ++itTime) {
+		       lsErrorTime.push_back(*itTime);
+		    }
+		    return;
+               }
+               else {
+                   lsErrorTime.clear();
+               }
+           }
+	}	        
+
+	log("%s request login.", strDomain.c_str());
+
+	IM::BaseDefine::UserInfo cUser;
+
+	if(g_loginStrategy.doLogin(strDomain, strPass, cUser))
+	{
+		IM::BaseDefine::UserInfo* pUser = msgResp.mutable_user_info();
+		pUser->set_user_id(cUser.user_id());
+		pUser->set_user_gender(cUser.user_gender());
+		pUser->set_department_id(cUser.department_id());
+		pUser->set_user_nick_name(cUser.user_nick_name());
+		pUser->set_user_domain(cUser.user_domain());
+		pUser->set_avatar_url(cUser.avatar_url());
+
+		pUser->set_email(cUser.email());
+		pUser->set_user_tel(cUser.user_tel());
+		pUser->set_user_real_name(cUser.user_real_name());
+		pUser->set_status(0);
+
+		pUser->set_sign_info(cUser.sign_info());
+
+		msgResp.set_result_code(0);
+		msgResp.set_result_string("成功");
+
+		//如果登陆成功，则清除错误尝试限制
+		CAutoLock cAutoLock(&g_cLimitLock);
+		list<uint32_t>& lsErrorTime = g_hmLimits[strDomain];
+		lsErrorTime.clear();
+	}
+	else
+	{
+		//密码错误，记录一次登陆失败
+		uint32_t tmCurrent = time(NULL);
+		CAutoLock cAutoLock(&g_cLimitLock);
+		list<uint32_t>& lsErrorTime = g_hmLimits[strDomain];
+		lsErrorTime.push_front(tmCurrent);
+
+		log("get result false");
+		msgResp.set_result_code(1);
+		msgResp.set_result_string("用户名/密码错误");
+	}
     }
     else
     {
-        msgResp.set_result_code(2);
-        msgResp.set_result_string("服务端内部错误");
+	    msgResp.set_result_code(2);
+	    msgResp.set_result_string("服务端内部错误");
     }
-    
-    
+
+
     pPduResp->SetPBMsg(&msgResp);
     pPduResp->SetSeqNum(pPdu->GetSeqNum());
     pPduResp->SetServiceId(IM::BaseDefine::SID_OTHER);
